d7ed038465259cb8891caf79c00f67d9f397a8da
mboard8070 <56896278+mboard8070@users.noreply.github.com>
2023-03-22 10:06:17 -0400

    update Stacked Material

--- masterMaterialStacked.py
+++ masterMaterialStacked.py
@@ -1,29 +1,36 @@
 import unreal
 
-AssetMatTools = unreal.AssetToolsHelpers.get_asset_tools()
-MaterialEditingLibrary = unreal.MaterialEditingLibrary
+AssetTools = unreal.AssetToolsHelpers.get_asset_tools()
+MaterialEditLibrary = unreal.MaterialEditingLibrary
 EditorAssetLibrary = unreal.EditorAssetLibrary
 
+masterMaterialStacked = AssetTools.create_asset("M_Master_Stacked", "/Game/masterMaterials", unreal.Material, unreal.MaterialFactoryNew())
 
-masterMaterial = AssetMatTools.create_asset("masterMatStacked_ORM", "/Game/Materials", unreal.Material, unreal.MaterialFactoryNew())
+#Create 2D Texture Param and Connect to Base Color
+baseColorTextureParam = MaterialEditLibrary.create_material_expression(masterMaterialStacked, unreal.MaterialExpressionTextureSampleParameter2D, -384, -300)
+MaterialEditLibrary.connect_material_property(baseColorTextureParam, "RGB", unreal.MaterialProperty.MP_BASE_COLOR)
 
-#Texture Parameter
-diffuseTextureParameter = MaterialEditingLibrary.create_material_expression(masterMaterial, unreal.MaterialExpressionTextureSampleParameter, -384, -200)
-#Connect to Material Channel
-MaterialEditingLibrary.connect_material_property(diffuseTextureParameter, "RGB", unreal.MaterialProperty.MP_BASE_COLOR)
+#Create Constant Value and Connect to Specular
+specParam = MaterialEditLibrary.create_material_expression(masterMaterialStacked, unreal.MaterialExpressionConstant, -250, 70)
+specParam.set_editor_property("R", 0.3)
+MaterialEditLibrary.connect_material_property(specParam, "", unreal.MaterialProperty.MP_SPECULAR)
 
-specParameter = MaterialEditingLibrary.create_material_expression(masterMaterial, unreal.MaterialExpressionConstant, -125, 70)
-specParameter.set_editor_property('R', 0.2)
-MaterialEditingLibrary.connect_material_property(specParameter, "", unreal.MaterialProperty.MP_SPECULAR)
+#Create 2D Texture Param and Connect to Normal
+normalTextureParam = MaterialEditLibrary.create_material_expression(masterMaterialStacked, unreal.MaterialExpressionTextureSampleParameter2D, -384, -200)
+MaterialEditLibrary.connect_material_property(normalTextureParam, "RGB", unreal.MaterialProperty.MP_NORMAL)
 
-normalTextureParameter = MaterialEditingLibrary.create_material_expression(masterMaterial, unreal.MaterialExpressionTextureSampleParameter, -125, 100)
-MaterialEditingLibrary.connect_material_property(normalTextureParameter, "RGB", unreal.MaterialProperty.MP_NORMAL)
+#Create 2D Texture Param and Connect to Occlusion, Roughness, and Metallic
+ormTextureParam = MaterialEditLibrary.create_material_expression(masterMaterialStacked, unreal.MaterialExpressionTextureSampleParameter2D, -384, 100)
+MaterialEditLibrary.connect_material_property(ormTextureParam, "R",  unreal.MaterialProperty.MP_AMBIENT_OCCLUSION)
+MaterialEditLibrary.connect_material_property(ormTextureParam, "G", unreal.MaterialProperty.MP_ROUGHNESS)
+MaterialEditLibrary.connect_material_property(ormTextureParam, "B", unreal.MaterialProperty.MP_METALLIC)
+ormTextureParam.rename("ORM")
 
-ormTextureParameter = MaterialEditingLibrary.create_material_expression(masterMaterial, unreal.MaterialExpressionTextureSampleParameter, -125, 100)
-MaterialEditingLibrary.connect_material_property(ormTextureParameter, "R", unreal.MaterialProperty.MP_AMBIENT_OCCLUSION)
-MaterialEditingLibrary.connect_material_property(ormTextureParameter, "G", unreal.MaterialProperty.MP_ROUGHNESS)
-MaterialEditingLibrary.connect_material_property(ormTextureParameter, "B", unreal.MaterialProperty.MP_METALLIC)
+#Create Material Instance
+stackedMatInstance = AssetTools.create_asset("masterMatStacked_ORM_Inst", "/Game/masterMaterials", unreal.MaterialInstanceConstant, unreal.MaterialInstanceConstantFactoryNew())
+MaterialEditLibrary.set_material_instance_parent(stackedMatInstance, masterMaterialStacked)
+MaterialEditLibrary.update_material_instance(stackedMatInstance)
 
-#create material instance
-masterMaterialInstance = AssetMatTools.create_asset("masterMatStacked_ORM_Inst", "/Game/Materials", unreal.MaterialInstanceConstant, unreal.MaterialInstanceConstantFactoryNew())
 
+EditorAssetLibrary.save_asset("/Game/masterMaterials/M_Master_Stacked", True)
+EditorAssetLibrary.save_asset("/Game/masterMaterials/masterMatStacked_ORM_Inst", True)
